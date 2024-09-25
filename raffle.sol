a80e1126b89eec04d9b631ec97d45014bfd4b1b1
Keeyki <yassine.chtioui34@gmail.com>
2024-09-25 17:26:51 +0200

    first commit

---
+++ raffle.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+/**
+ *@title A simple Raffle contract
+ *@author Yassine Chtioui
+ *@notice This is contract is for creating a simple raffle
+ *@dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffle {
+    function etnerRaffle() public {}
+
+    function pickWinner() public {}
+}
